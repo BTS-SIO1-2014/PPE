d0131e518f209929ae072fb05c0f5d4d0fd1c09f
nicopeva <nicopaucant@hotmail.fr>
2014-04-09 14:28:59 +0200

    page commander

---
+++ SiteWeb/Accueil/commander.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<?php
+	$TITRE = "Commander";
+	include("header.php");
+?>
+
+    <!-- Begin page content -->
+    <div class="container">
+      <div class="page-header">
+	  </br>
+	  </br>
+	  </br>
+	  </br>
+        <h1>Bon de commande</h1>
+      </div>
+      <p class="lead">Récapitulation de la Commande</p>
+	  <a class="btn btn-lg btn-success" href="CommandeValider.html" role="button">Valider La Commande</a>
+	  <a class="btn btn-lg btn-success" href="../Accueil.html" role="button">Annuler</a>
+		</br>
+    </div>
+
+
+<table id="MaTable" class="tabcenter">
+    <tr>
+       <th>Numero et date</th>
+       <th>Produit</th>
+       <th>Quantite</th>
+	   <th>TVA</th>
+	   <th>Prix</th>
+	   <th>Date livraison Prevue</th>
+	   <th>Durée</th>
+   </tr>
+   <tr>
+       <td>..........</td>
+       <td>..........</td>
+       <td>..........</td>
+	   <td>..........</td>
+	   <td>..........</td>
+	   <td>..........</td>
+	   <td>..........</td>
+   </tr>
+   <tr>
+       <td>..........</td>
+       <td>..........</td>
+       <td>..........</td>
+	   <td>..........</td>
+	   <td>..........</td>
+	   <td>..........</td>
+	   <td>..........</td>
+   </tr>
+   <tr>
+       <td>..........</td>
+       <td>..........</td>
+       <td>..........</td>
+	   <td>..........</td>
+	   <td>..........</td>
+	   <td>..........</td>
+	   <td>..........</td>
+   </tr>
+   <tr>
+       <td>..........</td>
+       <td>..........</td>
+       <td>..........</td>
+	   <td>..........</td>
+	   <td>..........</td>
+	   <td>..........</td>
+	   <td>..........</td>
+   </tr>
+</html>
